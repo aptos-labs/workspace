1fdc1bc64512bb00b3153ad669d5e112533e24c9
maayan <maayansavir@gmail.com>
2024-08-27 18:35:10 -0400

    implement Mocha root hooks to run before and after each test suite

--- workspace/src/internal/rootHooks.ts
+++ workspace/src/internal/rootHooks.ts
@@ -1,19 +1,10 @@
-export const mochaHooks = {
-  before: async function () {
-    const { LocalNode } = await import(
-      "@aptos-labs/ts-sdk/dist/common/cli/index.js"
-    );
-    const localNode = new LocalNode();
-    try {
-      await localNode.run();
-      (globalThis as any).__LOCAL_NODE__ = localNode;
-    } catch (err: any) {
-      console.log("err starting localnet", err);
-    }
+import { RootHookObject } from "mocha";
+
+export const mochaHooks: RootHookObject = {
+  beforeAll: async function () {
+    // code to run before each test suite
   },
-  after: async function () {
-    if ((globalThis as any).__LOCAL_NODE__?.process) {
-      await (globalThis as any).__LOCAL_NODE__.stop();
-    }
+  afterAll: async function () {
+    // code to run after each test suite
   },
 };
