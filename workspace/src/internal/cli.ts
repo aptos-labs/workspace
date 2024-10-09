ba1ccbb8a534013c277b0d666ef8281a8a573955
maayan <maayansavir@gmail.com>
2024-10-09 19:29:34 -0400

    add surf support

--- workspace/src/internal/cli.ts
+++ workspace/src/internal/cli.ts
@@ -1,6 +1,8 @@
 import { program } from "commander";
 import prompts from "prompts";
+
 import { test, init } from "../tasks";
+import { genAbi } from "../tasks/gen-abi";
 
 export type PromptResult = {
   language: "ts" | "js";
@@ -28,6 +30,27 @@ program
   });
 
 program
+  .command("gen-abi")
+  .description("Generate the module ABI")
+  .option(
+    "--names <NAMES>",
+    "The names you use in the named-addresses for the move binary, Example: alice,bob"
+  )
+  .action(async (options) => {
+    /**
+     * NOTE: The only feasible way to generate the ABI is to publish the package to chain
+     * and fetch the ABI from the node.
+     *
+     * This is because the Aptos compiler does not support generating the ABI in a JSON format
+     * from a local Move binary.
+     *
+     * There is a work effort to add that support to the compiler, once it exists, this
+     * command will be changed.
+     */
+    await genAbi(options);
+  });
+
+program
   .command("test")
   .description("Run unit tests")
   .option(
@@ -43,3 +66,19 @@ program
   });
 
 program.parse();
+
+function buildAddressObject(
+  input: string,
+  address: string
+): Record<string, string> {
+  // Split the input string by commas to get the names
+  const names = input.split(",");
+
+  // Build the object by mapping each name to the given address
+  const result: Record<string, string> = {};
+  names.forEach((name) => {
+    result[name] = address;
+  });
+
+  return result;
+}
---
+++ workspace/src/tasks/gen-abi.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+
+import { publishPackageTask } from "../tasks/publish";
+import { generateTestAccount } from "../external";
+
+export const genAbi = async (options: { names: string }) => {
+  const localNode = new cli.LocalNode();
+  // spin up a localnet
+  await localNode.run();
+  // create a random account
+  const publisher = await generateTestAccount();
+  // build the named addresses object with the random account address
+  const parsedNamedAddresses = buildAddressObject(
+    options.names,
+    publisher.accountAddress.toString()
+  );
+  // publish the package to chain
+  const packageObjectAddress = await publishPackageTask({
+    publisher,
+    namedAddresses: parsedNamedAddresses,
+    addressName: Object.keys(parsedNamedAddresses)[0],
+  });
+  // fetch the abi from the node and generate in a local file
+  await fetchAbiFromNode(packageObjectAddress);
+  // stop the localnet
+  await localNode.stop();
+};
+
+export const fetchAbiFromNode = async (objectAddress: string) => {
+  const aptosConfig = new AptosConfig({ network: Network.LOCAL });
+  const aptos = new Aptos(aptosConfig);
+  const modules = await aptos.getAccountModules({
+    accountAddress: objectAddress,
+  });
+  modules.forEach((module) => {
+    const abi = module.abi;
+    if (!abi) {
+      return;
+    }
+    const abiString = `export const ${abi.name.toUpperCase()}_ABI = ${JSON.stringify(abi)} as const;`;
+    fs.mkdirSync("abis", { recursive: true });
+    fs.writeFileSync(`abis/${abi.name}_abi.ts`, abiString);
+  });
+};
+
+function buildAddressObject(
+  input: string,
+  address: string
+): Record<string, string> {
+  // Split the input string by commas to get the names
+  const names = input.split(",");
+
+  // Build the object by mapping each name to the given address
+  const result: Record<string, string> = {};
+  names.forEach((name) => {
+    result[name] = address;
+  });
+
+  return result;
+}
